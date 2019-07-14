4a4bbbba62a86cdee9ad75986346ea71397388fa
Charles Wolfe <c.wolfe132@gmail.com>
2019-07-14 15:50:11 -0700

    IR sensor and remote working

--- alarm_code.ino
+++ alarm_code.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <IRremote.h>
 
+//IR sensor pin
+const int RECV_PIN = 4;
+
+//Define IR receiver and Results objects
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+//const int ledPin = 6;
+
+void setup() {
+  //Serial monitor at 9600 baud
+  Serial.begin(9600);
+  //Enable the IR receiver
+  irrecv.enableIRIn();
+  
+  //pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (irrecv.decode(&results)){
+    // Print Code in HEX
+    Serial.println(results.value, HEX);
+    irrecv.resume();
+  }
 }
