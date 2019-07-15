9de2c111f66823a2164b1279226dad7ccf2c3b6b
Charles Wolfe <c.wolfe132@gmail.com>
2019-07-14 22:32:25 -0700

    ultrasonic sensor working with LED and IR sensor

--- alarm_code.ino
+++ alarm_code.ino
@@ -1,5 +1,13 @@
 #include <IRremote.h>
 
+//Ultrasonic sensor pins
+const int trigPin = 9;
+const int echoPin = 10;
+
+//Variables for the ultrasonic sensor
+long duration;
+int cm;
+
 //IR sensor pin
 const int RECV_PIN = 4;
 
@@ -14,6 +22,9 @@ int togglestate = 0;
 void setup() {
   //Serial monitor at 9600 baud
   Serial.begin(9600);
+  //Set trig to output and echo to input
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   //Enable the IR receiver
   irrecv.enableIRIn();
   //Set LED pins as outputs
@@ -21,6 +32,10 @@ void setup() {
 }
 
 void loop() {
+  if (togglestate == 1){
+    get_distance();
+   }
+  
   if (irrecv.decode(&results)){
     if (results.value == 0xFF38C7){
       if (togglestate == 0){
@@ -34,4 +49,28 @@ void loop() {
     }
     irrecv.resume(); 
   }  
+}
+
+long ms_to_cm(long ms){
+  return ms*0.034/2;
+}
+
+void get_distance(){
+   //clear the trig pin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  //set the trigPin on HIGH for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  //read the echoPin, return the sound wave travel time in ms
+  duration = pulseIn(echoPin, HIGH);
+
+  //calculate the distance
+  cm = ms_to_cm(duration);
+
+  Serial.print("Distance: ");
+  Serial.println(cm);
 }
