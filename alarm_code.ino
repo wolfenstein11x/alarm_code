1c2229a7b0959785557829eeeb1ae3a348d35fd1
Charles Wolfe <c.wolfe132@gmail.com>
2019-07-14 18:22:42 -0700

    IR sensor and LED working together

--- alarm_code.ino
+++ alarm_code.ino
@@ -7,21 +7,31 @@ const int RECV_PIN = 4;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
-//const int ledPin = 6;
+const int bluePin = 6;
+
+int togglestate = 0;
 
 void setup() {
   //Serial monitor at 9600 baud
   Serial.begin(9600);
   //Enable the IR receiver
   irrecv.enableIRIn();
-  
-  //pinMode(ledPin, OUTPUT);
+  //Set LED pins as outputs
+  pinMode(bluePin, OUTPUT);
 }
 
 void loop() {
   if (irrecv.decode(&results)){
-    // Print Code in HEX
-    Serial.println(results.value, HEX);
-    irrecv.resume();
-  }
+    if (results.value == 0xFF38C7){
+      if (togglestate == 0){
+        digitalWrite(bluePin, HIGH);
+        togglestate = 1;
+      }
+      else {
+        digitalWrite(bluePin, LOW);
+        togglestate = 0;
+      }
+    }
+    irrecv.resume(); 
+  }  
 }
