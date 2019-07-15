4e7e35b2741abd673f0dac1df8f29bbdf81d9713
Charles Wolfe <c.wolfe132@gmail.com>
2019-07-15 16:33:28 -0700

    Added button for alarm

--- alarm_code.ino
+++ alarm_code.ino
@@ -1,12 +1,18 @@
 #include <IRremote.h>
+#include <math.h>
 
 //Ultrasonic sensor pins
 const int trigPin = 9;
 const int echoPin = 10;
+//Distance Ultrasonic sensor is looking for (in cm)
+const int target_d = 5;
 
 //Variables for the ultrasonic sensor
 long duration;
 int cm;
+
+//Variables and constants for the buzzer
+int alarm_toggle = 0;
 
 //IR sensor pin
 const int RECV_PIN = 4;
@@ -34,6 +40,17 @@ void setup() {
 void loop() {
   if (togglestate == 1){
     get_distance();
+    if (irrecv.decode(&results)){
+      if (results.value == 0xFF6897){
+        alarm_toggle = 1;
+        Serial.println("Alarm ready");
+      }
+    }
+    if (alarm_toggle == 1){
+      if (abs(get_distance() - target_d) > 1){
+        Serial.println("BEEP BEEP!");
+      }
+    }
    }
   
   if (irrecv.decode(&results)){
@@ -48,14 +65,15 @@ void loop() {
       }
     }
     irrecv.resume(); 
-  }  
+  }
+  delay(500);  
 }
 
 long ms_to_cm(long ms){
   return ms*0.034/2;
 }
 
-void get_distance(){
+int get_distance(){
    //clear the trig pin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -70,7 +88,9 @@ void get_distance(){
 
   //calculate the distance
   cm = ms_to_cm(duration);
-
+ 
   Serial.print("Distance: ");
   Serial.println(cm);
+
+  return cm;
 }
